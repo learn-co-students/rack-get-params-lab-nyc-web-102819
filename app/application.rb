19eed9f53ce58114085e51b5d6e9d62ff234a003
MarioBland <gabedotgutierrez@gmail.com>
2019-11-18 15:32:08 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,30 +1,64 @@
+
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+     resp = Rack::Response.new
+     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
+     if req.path.match(/items/)
+       @@items.each do |item|
+         resp.write "#{item}\n"
+       end
+     elsif req.path.match(/search/)
+       search_term = req.params["q"]
+       resp.write handle_search(search_term)
+     elsif req.path.match(/cart/)
+          if @@cart == []
+            resp.write "Your cart is empty"
+          else
+            @@cart.each do |cart|
+            resp.write "#{cart}\n"
+          end
+        end  
+      elsif req.path.match(/add/)
+        itemadd = req.params["item"] #<-NEVER HEARD OF REQ OR PARAMS I GOTTA STUDY THIS
+          if @@items.include?(itemadd)
+            @@cart << itemadd
+            resp.write "added #{itemadd}\n"
+          else
+            resp.write "We don't have that item"
+          end
+      else
       resp.write "Path Not Found"
     end
-
-    resp.finish
+     resp.finish
   end
+  
+  # def call(env)
+  #   resp = Rack::Response.new
+  #   req = Rack::Request.new(env)
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
+  #   elsif req.path.match(/cart/)
+  #     if @@cart == []
+  #       resp.write "Your cart is empty"
+  #     else
+  #       @@cart.each do |cart|
+  #       resp.write "#{cart}\n"
+  #         end
+  #     end
+  # resp.finish
+  # end
+  # end
+
+   def handle_search(search_term)
+     if @@items.include?(search_term)
+       return "#{search_term} is one of our items"
+     else
+       return "Couldn't find #{search_term}"
+     end
+   end
+
 end
